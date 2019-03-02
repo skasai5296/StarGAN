e20ea8ccc552f8150c7c7dd34c3271e475fc5ea4
skasai5296 <seito5296@gmail.com>
2019-03-02 21:24:43 +0900

    fixed train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-env CUDA_VISIBLE_DEVICES=0,3,4 nohup python main.py --mode=train --num_workers=2 --batch_size=8 --dataset=Both --image_size=256 --c_dim=5 --c2_dim=8 --sample_dir=stargan_both/samples \
---log_dir=stargan_both/logs --model_save_dir=stargan_both/models --result_dir=stargan_both/results --celeba_image_dir='../dsets/CelebA/' --rafd_image_dir='' \
+env CUDA_VISIBLE_DEVICES=0,1 nohup python main.py --mode=train --num_workers=2 --batch_size=8 --dataset=Both --image_size=256 --c_dim=5 --c2_dim=8 --sample_dir=stargan_both/samples \
+--log_dir=stargan_both/logs --model_save_dir=stargan_both/models --result_dir=stargan_both/results --celeba_image_dir='dsets/CelebA/images' --rafd_image_dir='dsets/RafD-aligned/train' \
 > out.log &!
